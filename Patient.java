20e60edbaf6695dc9b841a1d13ce1303a2e3db8f
Pierce Frawley <pierce@procore.com>
2015-03-07 13:56:40 -0800

    Merge Me

--- Patient.java
+++ Patient.java
@@ -3,14 +3,14 @@ import java.util.Date;
 
 public class Patient {
 private String patientid;
-private String patientrole;
-private String givenname;
-private String familyname;
-private String suffix;
-private String gender;
-private Date birthtime;
-private String providerId;
-private Date xmlCreationdate;
+//private String patientrole;
+//private String givenname;
+//private String familyname;
+//private String suffix;
+//private String gender;
+//private Date birthtime;
+//private String providerId;
+//private Date xmlCreationdate;
 public Patient(String ID, String firstName, String givenname, String familyname, String suffix, String gender, Date birthtime, String providerid, Date xmlCreationDate)
 {
 this.patientid = ID;
