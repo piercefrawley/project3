c2e868b5114198e45e90b28feca5a00ab1660a2a
Pierce Frawley <pierce@procore.com>
2015-03-10 22:10:45 -0700

    Author Class

---
+++ src/Package/Author.java
@@ -0,0 +1,69 @@
+package Package;
+
+import java.util.Date;
+
+public class Author {
+	public String AuthorId;
+	public String PatientId;
+	public String AuthorTitle;
+	public String AuthorFirstName;
+	public String AuthorLastName;
+	public String ParticipatingRole;
+
+	public Author(String aid, String pid, String title, String fname, String lname, String role) {
+		this.AuthorId = aid;
+		this.PatientId = pid;
+		this.AuthorTitle = title;
+		this.AuthorFirstName = fname;
+		this.AuthorLastName = lname;
+		this.ParticipatingRole = role;
+	}
+	
+	String getAuthorId() {
+		return AuthorId;
+	}
+
+	void setAuthorId(String authorId) {
+		AuthorId = authorId;
+	}
+
+	String getPatientId() {
+		return PatientId;
+	}
+
+	void setPatientId(String patientId) {
+		PatientId = patientId;
+	}
+
+	String getAuthorTitle() {
+		return AuthorTitle;
+	}
+
+	void setAuthorTitle(String authorTitle) {
+		AuthorTitle = authorTitle;
+	}
+
+	String getAuthorFirstName() {
+		return AuthorFirstName;
+	}
+
+	void setAuthorFirstName(String authorFirstName) {
+		AuthorFirstName = authorFirstName;
+	}
+
+	String getAuthorLastName() {
+		return AuthorLastName;
+	}
+
+	void setAuthorLastName(String authorLastName) {
+		AuthorLastName = authorLastName;
+	}
+
+	String getParticipatingRole() {
+		return ParticipatingRole;
+	}
+
+	void setParticipatingRole(String participatingRole) {
+		ParticipatingRole = participatingRole;
+	}
+}
